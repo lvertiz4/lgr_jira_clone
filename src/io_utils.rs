26edbfbe7b571d4c6e79b23d449e9f5eb543eb2c
lvertiz4 <lvertiz@gmail.com>
2023-05-10 21:41:55 -0400

    added code blocks for prompt functions

--- src/io_utils.rs
+++ src/io_utils.rs
@@ -1,9 +1,10 @@
 use std::io;
 
 pub fn get_user_input() -> String {
-    let mut user_input = String::new();
+    let mut user_input = String::new(); //user_input is a variable that will hold the input from the user input
 
-    io::stdin().read_line(&mut user_input).unwrap();//:stdin() is a function that constructs a new handle to the standard input of the current process.Each handle returned is a reference to a shared global buffer whose access is synchronized via a mutex.
-
+    io::stdin().read_line(&mut user_input).unwrap();//A handle to the standard input stream of a process. 
+    //::stdin() is a function that constructs a new handle to the standard input of the current process. Each handle returned is a reference to a shared global buffer whose access is synchronized via a mutex.
+    //1)take input with io::stdin() 2) .read_line() ocks this handle and reads a line of input, appending it to the specified buffer 3) .read_line() returns a Result, from the which value is take using .unwrap(
     user_input
 }
--- src/ui/prompts.rs
+++ src/ui/prompts.rs
@@ -21,21 +21,63 @@ impl Prompts {
 }
 
 fn create_epic_prompt() -> Epic {
-    
-
+    println!("----------------------------");
+    println!("Epic Name: ");
+    let epic_name = get_user_input();//create variable to store first user input
+    println!("Epic Description: ");
+    let epic_desc = get_user_input();
+    let epic = Epic::new(epic_name.trim().to_string(), epic_desc.trim().to_owned());//instantiate new Epic with user input assigned to particular struct fields
+    epic
 }
 
 fn create_story_prompt() -> Story {
-
+ println!("----------------------------");
+ println!("Story Name: ");
+ let story_name = get_user_input();
+ println!("Story Description: ");
+ let story_desc = get_user_input();
+ let story = Story::new(story_name.trim().to_string(), story_desc.trim().to_owned());
+ story
 
 }
 fn delete_epic_prompt() -> bool {
-    
+    println!("----------------------------");
+    println!("Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/N]:");
+    let input = get_user_input();
+    if input.trim().eq("Y") {//.eq() method tests for self and other values to be equal, and is used by ==
+
+        return true;
+    }
+    false
 }
 
 fn delete_story_prompt() -> bool {
-    
+    println!("----------------------------");
+    println!("Are you sure you want to delete this story? [Y/N]:");
+    let input = get_user_input();
+    if input.trim().eq("Y") {
+        return true;
+    }
+    false
 }
-fn create_epic_prompt() -> Option<Status>{
-
+fn update_status_prompt() -> Option<Status>{
+    prinln!("----------------------------");
+    println!("New Status (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED");
+    let input = get_user_input();
+    let input = input.trim().parse::<u8>(); //prase function turns &str into u8 integers with tuberfish syntax
+    match input {
+        1 => {
+            return Some(Status::Open);
+        }
+        2 => {
+            return Some(Status::InProgress);
+        }  
+        3 => {
+            return Some(Status::Resolved);
+        }
+        4 => {
+            return Some(Status::Closed);
+        }
+    }
+    None //Don't forget to return None with a Option
 }
