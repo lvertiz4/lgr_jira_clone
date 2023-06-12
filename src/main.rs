7bceeaf97f36071c516fdab92b5128f913b2ea12
lvertiz4 <lvertiz@gmail.com>
2023-06-12 13:44:41 -0400

    JIRA Clone Stage 3 Step 2 complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::rc::Rc;
 
-
 mod models;
 mod db;
 use db::*;
@@ -23,18 +22,17 @@ fn main() {
 
         //TODO: implemnt the following functionality
         //1. get current page from the Navigator
-        if let Some(page) = navigator.get_current_page() {
+        if let Some(page) = navigator.get_current_page() {//page is a Box pointer to any object that contains the Page trait, inferred by call to get_current_page() method for Navigator struct; recall box pointers allocate values on the heap
             //2. render page
             if let Err(error) = page.draw_page() {
                 println!("Error rendering page: {}\nPress any key to continue...", error);
                 wait_for_key_press();
-            }
-        }
+            };
         //3. get user input
         let user_input = get_user_input();
 
           //4. pass input to page's input handler    
-        match page.handle_input(user_input.trim()) {
+        match page.handle_input(user_input.trim()) { //handle_input is a method for Navigator struct; therefore compiler infers page relates to pages field of Navigator struct, which has Page trait objects in a vector
             Err(error) => {
                 println!("Error getting user input: {}\nPress any key to continue...", error);
                 wait_for_key_press();
@@ -48,8 +46,9 @@ fn main() {
                     }
                 }
             }
-        } else {
-            break;//recall that 'break' is a keyword to exit early from a loop, in this case, the loop to render the screen
+        }
+    } else {
+        break;//recall that 'break' is a keyword to exit early from a loop, in this case, the loop to render the screen
         }
     }
 }
